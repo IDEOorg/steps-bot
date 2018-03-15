4df3f6a1a42c54d852b4b6741b6ad0cdbb765c26
Michael Zhong <michael@ideo.org>
2018-03-15 14:38:06 -0400

    sample convo starter

--- index.js
+++ index.js
@@ -16,6 +16,16 @@ controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
   })
 })
 
-controller.hears('*', 'message_received', (bot, message) => {
+controller.hears(['hi', 'hello'], 'message_received', (bot, message) => {
+  bot.startConversation(message, (err, convo) => {
+    convo.say('Hi, I am Oliver, an SMS bot! :D')
+    convo.ask('What is your name?', (res, convo) => {
+      convo.say(`Nice to meet you, ${res.text}!`)
+      convo.next()
+    })
+  })
+})
+
+controller.hears('.*', 'message_received', (bot, message) => {
   bot.reply(message, 'huh?')
 })
