52b3965d27a737c44911c1aada0261e482b222a9
Michael Zhong <michael@ideo.org>
2018-03-20 18:13:04 -0400

    name bob

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ firebase.auth()
 
    controller.hears(['hi', 'hello'], 'message_received', (bot, message) => {
      bot.startConversation(message, (err, convo) => {
-       convo.say('Hi, I am Oliver, an SMS bot! :D')
+       convo.say('Hi, I am Bob, an SMS bot! :D')
        convo.ask('What is your name?', (res, convo) => {
          convo.say(`Nice to meet you, ${res.text}!`)
          convo.next()
