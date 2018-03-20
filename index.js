25cab0c2862086a9ab505662b0b20847154f0f07
Michael Zhong <michael@ideo.org>
2018-03-20 18:10:56 -0400

    storage added

--- index.js
+++ index.js
@@ -1,31 +1,59 @@
 require('dotenv').config()
 const Botkit = require('botkit')
+var firebase = require("firebase")
 
-const controller = Botkit.twiliosmsbot({
-    account_sid: process.env.TWILIO_ACCOUNT_SID,
-    auth_token: process.env.TWILIO_AUTH_TOKEN,
-    twilio_number: process.env.TWILIO_NUMBER,
-    debug: true
-})
+var config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "bedstuy-bdf4e.firebaseapp.com",
+  databaseURL: "https://bedstuy-bdf4e.firebaseio.com",
+  projectId: "bedstuy-bdf4e",
+  storageBucket: "bedstuy-bdf4e.appspot.com"
+}
+firebase.initializeApp(config)
 
-const bot = controller.spawn({});
+firebase.auth()
+.signInWithEmailAndPassword(process.env.FIREBASE_EMAIL, process.env.FIREBASE_PASSWORD)
+.then(initBotkit)
 
-controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
-  controller.createWebhookEndpoints(controller.webserver, bot, function () {
-    console.log('TwilioSMSBot is online!')
-  })
-})
+ function initBotkit() {
+   firebaseStorage = require('botkit-storage-firebase')(config, {
+     email: process.env.FIREBASE_EMAIL,
+     password: process.env.FIREBASE_PASSWORD
+   })
+   firebaseStorage.then(function (d) {
+     setupBotkit(d)
+   })
+ }
 
-controller.hears(['hi', 'hello'], 'message_received', (bot, message) => {
-  bot.startConversation(message, (err, convo) => {
-    convo.say('Hi, I am Oliver, an SMS bot! :D')
-    convo.ask('What is your name?', (res, convo) => {
-      convo.say(`Nice to meet you, ${res.text}!`)
-      convo.next()
-    })
-  })
-})
+ function setupBotkit(storageModule) {
+   const controller = Botkit.twiliosmsbot({
+       account_sid: process.env.TWILIO_ACCOUNT_SID,
+       auth_token: process.env.TWILIO_AUTH_TOKEN,
+       twilio_number: process.env.TWILIO_NUMBER,
+       debug: true,
+       storage: storageModule
+   })
 
-controller.hears('.*', 'message_received', (bot, message) => {
-  bot.reply(message, 'huh?')
-})
+   const bot = controller.spawn({});
+
+   controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
+     controller.createWebhookEndpoints(controller.webserver, bot, function () {
+       console.log('TwilioSMSBot is online!')
+     })
+     controller.storage.users.save({id: 'bagel', foo:'bar'})
+   })
+
+   controller.hears(['hi', 'hello'], 'message_received', (bot, message) => {
+     bot.startConversation(message, (err, convo) => {
+       convo.say('Hi, I am Oliver, an SMS bot! :D')
+       convo.ask('What is your name?', (res, convo) => {
+         convo.say(`Nice to meet you, ${res.text}!`)
+         convo.next()
+       })
+     })
+   })
+
+   controller.hears('.*', 'message_received', (bot, message) => {
+     bot.reply(message, 'huh?')
+   })
+ }
