64636df8ad7638ef773b5539ec955be411079295
Michael Zhong <michael@ideo.org>
2018-03-28 21:35:33 -0400

    removed botkit-storage, added eslint

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-require('dotenv').config()
-const Botkit = require('botkit')
-var firebase = require("firebase")
+require('dotenv').config();
+const Botkit = require('botkit');
+const firebase = require('firebase');
 
-var config = {
+const config = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: "bedstuy-bdf4e.firebaseapp.com",
-  databaseURL: "https://bedstuy-bdf4e.firebaseio.com",
-  projectId: "bedstuy-bdf4e",
-  storageBucket: "bedstuy-bdf4e.appspot.com"
-}
-firebase.initializeApp(config)
+  authDomain: 'bedstuy-bdf4e.firebaseapp.com',
+  databaseURL: 'https://bedstuy-bdf4e.firebaseio.com',
+  projectId: 'bedstuy-bdf4e',
+  storageBucket: 'bedstuy-bdf4e.appspot.com'
+};
+firebase.initializeApp(config);
 
-setupBotkit()
+setupBotkit();
 
- function setupBotkit() {
-   const controller = Botkit.twiliosmsbot({
-       account_sid: process.env.TWILIO_ACCOUNT_SID,
-       auth_token: process.env.TWILIO_AUTH_TOKEN,
-       twilio_number: process.env.TWILIO_NUMBER,
-       debug: true
-   })
+function setupBotkit() {
+  const controller = Botkit.twiliosmsbot({
+    account_sid: process.env.TWILIO_ACCOUNT_SID,
+    auth_token: process.env.TWILIO_AUTH_TOKEN,
+    twilio_number: process.env.TWILIO_NUMBER,
+    debug: true
+  });
 
-   const bot = controller.spawn({});
+  const instance = controller.spawn({});
 
-   controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
-     controller.createWebhookEndpoints(controller.webserver, bot, function () {
-       console.log('TwilioSMSBot is online!')
-     })
-   })
+  controller.setupWebserver(process.env.PORT || 3000, () => {
+    controller.createWebhookEndpoints(controller.webserver, instance, () => {
+      console.log('TwilioSMSBot is online!');
+    });
+  });
 
-   initScript(bot, controller);
-}
-
-function initScript (bot, controller) {
   controller.hears('.*', 'message_received', (bot, message) => {
-    bot.createConversation(message, function (err, convo) {
-      convo.addMessage({text: 'Hi there! It\'s Roo, hope you\'re ready for your task. Let\'s get started.'}, 'day1');
+    bot.createConversation(message, (err, convo) => {
+      convo.addMessage({ text: 'Hi there! It\'s Roo, hope you\'re ready for your task. Let\'s get started.' }, 'day1');
       convo.say('Hi there! It\'s Roo, your financial chatbot assistant\n\nCongratulations on taking that first step and seeing a financial counselor!');
       convo.say('We\'ll be in touch tomorrow with your first task.');
-      convo.addQuestion({text: 'We\'ll be in touch tomorrow with your first task.'},function(res, convo) {
-        convo.setTimeout(10000);
-        convo.onTimeout(function(convo) {
-          convo.gotoThread('day1');
-          convo.next();
-        });
-      },{},'default');
       convo.activate();
     });
-  })
+  });
 }
