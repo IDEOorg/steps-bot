e6d94975878b4451db51c9c4918ed576f6b3c1c4
Michael Zhong <michael@ideo.org>
2018-05-02 23:25:33 -0400

    added body parser

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 module.exports = function server(fbController, twilioController) {
   const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.listen(process.env.PORT || 3000, null, () => {
   });
 
