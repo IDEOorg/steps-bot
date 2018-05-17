e40f67574a3f6874a26ee22119875259ff92e2f2
Michael Zhong <michael@ideo.org>
2018-05-17 12:07:47 -0400

    setting up server to host static image files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 module.exports = function server(fbController, twilioController) {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use('/static', express.static(path.join(__dirname, 'static')));
   app.listen(process.env.PORT || 3000, null, () => {
   });
 
