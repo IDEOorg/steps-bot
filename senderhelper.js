cc09b3192b5b72b55d5ef813772085b9b45808ed
Michael Zhong <michael@ideo.org>
2018-06-06 00:53:01 -0400

    debug change

--- senderhelper.js
+++ senderhelper.js
@@ -17,10 +17,11 @@ async function sendReply(platform, userId, messages) {
       await sendFBMessage(userId, formatMsgForFB(message)); // eslint-disable-line
     } else if (platform === 'sms') {
       const response = await sendSMSMessage(userId, formatMsgForSMS(message)); // eslint-disable-line
-      response.then((result) => {
-        console.log('resulttttt');
-        console.log(result);
-      });
+      console.log(response);
+      // response.then((result) => {
+      //   console.log('resulttttt');
+      //   console.log(result);
+      // });
       if (message.type === 'image') {
         await sleep(6000); // eslint-disable-line
       } else {
