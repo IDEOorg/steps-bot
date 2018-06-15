7caaef5bd031878429300b71a307f2bd795a86cc
Michael Zhong <michael@ideo.org>
2018-06-14 21:07:33 -0400

    populate db users created, seed tasks recurring fields inputted

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,18 +1,40 @@
 const rp = require('request-promise');
+const seedData = require('./seed.json');
+const seedMediaData = require('./seedmedia.json');
+const seedTasksData = require('./seedtasks.json');
+const url = 'http://localhost:3001';
 
+populateDB();
 
-const options = {
-  method: 'POST',
-  uri: 'https://localhost:3001/api/clients',
-  body: {
-    some: 'payload'
-  },
-  json: true // Automatically stringifies the body to JSON
-};
-
-rp(options)
-  .then((response) => {
-    console.log(response);
-  }).catch((err) => {
-    console.log(err);
+async function populateDB() {
+  const org = await rp({
+    method: 'POST',
+    uri: url + '/api/orgs',
+    body: seedData.orgs[0],
+    json: true // Automatically stringifies the body to JSON
   });
+  const orgId = org.id.id;
+  const coachData = seedData.coaches[0];
+  coachData.org_id = orgId;
+  const coach = await rp({
+    method: 'POST',
+    uri: url + '/api/coaches',
+    body: coachData,
+    json: true // Automatically stringifies the body to JSON
+  });
+  const coachId = coach.id.id;
+
+  const usersData = seedData.users;
+  for (let i = 0; i < usersData.length; i++) {
+    const userData = usersData[i];
+    userData.coach_id = coachId;
+    userData.org_id = orgId;
+    const user = await rp({ // eslint-disable-line
+      method: 'POST',
+      uri: url + '/api/clients',
+      body: userData,
+      json: true // Automatically stringifies the body to JSON
+    });
+    console.log(user);
+  }
+}
