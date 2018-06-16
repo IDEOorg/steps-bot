0f458e455de8da2b27ba720bf5ab147bd1604751
Michael Zhong <michael@ideo.org>
2018-06-16 10:20:48 -0400

    test.js to gitignore

--- test.js
+++
@@ -1,7 +0,0 @@
-const bot = require('./bothelper');
-
-bot.setupFirebase().then((db) => {
-  bot.getResponse(db, 'fb', '10', 'startprompt').then((response) => {
-    console.log(response);
-  });
-});
