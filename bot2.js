e86a0b43bf99a8e97d8aacc88d66a089a017e229
Michael Zhong <michael@ideo.org>
2018-06-16 20:14:07 -0400

    remove debug

--- bot2.js
+++ bot2.js
@@ -10,14 +10,12 @@ const server = require('./server.js');
 const fbController = Botkit.facebookbot({
   verify_token: process.env.FB_VERIFY_TOKEN,
   access_token: process.env.FB_PAGE_ACCESS_TOKEN,
-  require_delivery: true,
-  debug: true
+  require_delivery: true
 });
 const twilioController = Botkit.twiliosmsbot({
   account_sid: process.env.TWILIO_ACCOUNT_SID,
   auth_token: process.env.TWILIO_AUTH_TOKEN,
-  twilio_number: process.env.TWILIO_NUMBER,
-  debug: true
+  twilio_number: process.env.TWILIO_NUMBER
 });
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 // We are passing the controller object into our express server module
