bcb0896ef3ffaa76e946d2c85dd700b4d992e937
Michael Zhong <michael@ideo.org>
2018-06-18 08:46:57 -0400

    changed sleep times

--- senderhelper.js
+++ senderhelper.js
@@ -27,9 +27,9 @@ async function sendReply(platform, userPlatformId, messages) {
       formattedMsg = formatMsgForSMS(message);
       const response = await sendSMSMessage(userPlatformId, formattedMsg); // eslint-disable-line
       if (message.type === 'image') {
-        await sleep(3000); // eslint-disable-line
+        await sleep(2800); // eslint-disable-line
       } else {
-        await sleep(1000); // eslint-disable-line
+        await sleep(800); // eslint-disable-line
         api.createMessage(null, BOT_ID, client.id, formattedMsg.body);
       }
     }
