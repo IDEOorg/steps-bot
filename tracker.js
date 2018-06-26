d92d6d96a97a0853e76f7c3f3a893b5b9e914f04
Matthew Epler <mepler@ideo.org>
2018-06-26 14:24:26 -0400

    adds abstraction for body object in trackClientResponse

--- tracker.js
+++ tracker.js
@@ -26,10 +26,6 @@ exports.trackMediaClicked = function trackMediaClicked(req) {
 };
 
 // see buildContentUrl() in bothelper.js
-exports.trackClientResponse = function trackClientResponse() {
-  keen.recordEvent('clientResponse', {
-    userId,
-    topic,
-    text,
-  });
+exports.trackClientResponse = function trackClientResponse(body) {
+  keen.recordEvent('clientResponse', body);
 };
