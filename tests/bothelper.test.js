caee72222f6d7945a0326282d3033805cf29542c
Michael Zhong <michael@ideo.org>
2018-06-27 15:47:16 -0400

    added 20% test coverage for api/bothelper

--- tests/bothelper.test.js
+++ tests/bothelper.test.js
@@ -1,1 +1,13 @@
-console.log('heyhey');
+const bothelper = require('../bothelper');
+
+test('when an invalid phone number texts in, say you don\'t recognize phone number', () => {
+  bothelper.getResponse('sms', '3290', 'startprompt', 'welcome').then((response) => {
+    expect(response.messages[0].message).toContain('Sorry, we didn\'t recognize the phone number you sent this from');
+  });
+});
+
+test('when a valid phone number texts in with null topic, default to ', () => {
+  bothelper.getResponse('sms', '3290', 'startprompt', 'welcome').then((response) => {
+    expect(response.messages[0].message).toContain('Sorry, we didn\'t recognize the phone number you sent this from');
+  });
+});
