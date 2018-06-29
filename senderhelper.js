aa76d80af290bbd664406f604d807abd65c28d3f
Michael Zhong <michael@ideo.org>
2018-06-29 15:13:07 -0400

    changed sleep timers

--- senderhelper.js
+++ senderhelper.js
@@ -21,14 +21,14 @@ async function sendReply(platform, userPlatformId, messages, isUpdateMessage) {
     if (platform === 'fb') {
       formattedMsg = formatMsgForFB(message);
       await sendFBMessage(userPlatformId, formattedMsg, isUpdateMessage); // eslint-disable-line
-      await sleep(600); // eslint-disable-line
+      await sleep(300); // eslint-disable-line
     } else if (platform === 'sms') {
       formattedMsg = formatMsgForSMS(message);
       await sendSMSMessage(userPlatformId, formattedMsg); // eslint-disable-line
       if (message.type === 'image') {
-        await sleep(6100); // eslint-disable-line
+        await sleep(3100); // eslint-disable-line
       } else {
-        await sleep(1100); // eslint-disable-line
+        await sleep(800); // eslint-disable-line
         api.createMessage(null, BOT_ID, client.id, formattedMsg.body);
       }
     }
