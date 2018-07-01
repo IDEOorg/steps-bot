25d1b465602d2a3a538ddd072cd13ff935f1210a
Michael Zhong <michael@ideo.org>
2018-07-01 02:03:26 -0400

    added cors to server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
 const { trackMediaClicked } = require('./tracker');
 
 module.exports = function server(fbEndpoint, twilioController) {
   const app = express();
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use('/static', express.static(path.join(__dirname, 'static')));
