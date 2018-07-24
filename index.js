089370a520424f2b089ec788ef8aa280b66895d7
Michael Zhong <michael@ideo.org>
2018-07-24 13:51:29 -0400

    debugging

--- index.js
+++ index.js
@@ -38,7 +38,9 @@ function fbEndpoint(req, res) {
   // get message payload here for new users
   bot.getResponse('fb', userPlatformId, userMessage, null, fbNewUserPhone).then((response) => {
     console.log(response.variables);
-    sender.sendReply('fb', userPlatformId, response.messages).then().catch((e) => {
+    sender.sendReply('fb', userPlatformId, response.messages).then(() => {
+      console.log('sent');
+    }).catch((e) => {
       console.log(e);
     }).finally(() => {
       console.log('updated user fb');
@@ -59,7 +61,9 @@ twilioController.hears('.*', 'message_received', (_, message) => {
   bot.getResponse('sms', userPlatformId, userMessage).then((response) => {
     if (response !== null) {
       console.log(response.variables);
-      sender.sendReply('sms', userPlatformId, response.messages).then().catch((e) => {
+      sender.sendReply('sms', userPlatformId, response.messages).then(() => {
+        console.log('sent');
+      }).catch((e) => {
         console.log(e);
       }).finally(() => {
         updater.updateUserToDB(userPlatformId, 'sms', response.variables).then(() => {
