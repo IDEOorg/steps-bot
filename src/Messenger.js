de267887e9f5cd545ae91f5786505df277785cba
Michael Zhong <mzhong21@gmail.com>
2018-08-23 20:45:31 +0200

    logging

--- src/Messenger.js
+++ src/Messenger.js
@@ -23,6 +23,8 @@ module.exports = class Messenger {
       if (this.platform === constants.FB) {
         formattedMsg = formatMsgForFB(message);
         try {
+          console.log(this.messages);
+          console.log(this.client);
           console.log(formattedMsg, this.isMessageSentFromCheckIn);
           await sendFBMessage(this.userPlatformId, formattedMsg, this.isMessageSentFromCheckIn); // eslint-disable-line
           await sleep(300); // eslint-disable-line
