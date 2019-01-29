5c9af64d3e504a3f6d849a87b06b2d1039c70003
mbuvi <meshack.mbuvi@andela.com>
2019-01-29 18:19:15 +0300

    chore(documentation): update readme to reflect how to work with the bot locally (#20)
      - add a script for live server
      - update README.md
    
    [Finishes #162314887]

--- src/api.js
+++ src/api.js
@@ -1,43 +1,46 @@
-const constants = require('./constants');
-const rp = require('request-promise');
-const sgMail = require('@sendgrid/mail');
-const { trackMessageSent } = require('./tracker');
-require('dotenv').config();
+const constants = require ('./constants');
+const rp = require ('request-promise');
+const sgMail = require ('@sendgrid/mail');
+const {trackMessageSent} = require ('./tracker');
+require ('dotenv').config ();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const TOPICS = constants.TOPICS;
+const STATUS = constants.STATUS;
 
-async function getAllClients() {
+sgMail.setApiKey (process.env.SENDGRID_API_KEY);
+
+async function getAllClients () {
   let clients = null;
   try {
-    clients = await rp({
+    clients = await rp ({
       method: 'GET',
-      uri: process.env.API_URL + '/clients',
+      uri: `${process.env.API_URL}/clients`,
       headers: {
-        Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+        Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
       },
-      json: true
+      json: true,
     });
   } catch (e) {
-    console.log('getAllClients api method failed', e.message);
+    console.log ('getAllClients api method failed', e.message);
     if (e.statusCode === 401) {
       return constants.UNAUTHORIZED;
     }
-    sendErrorToZendesk(e);
+    sendErrorToZendesk (e);
   }
   return clients;
 }
 
-async function getOrgName(id) {
-  const org = await rp({
+async function getOrgName (id) {
+  const org = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/orgs/' + id.toString(),
+    uri: `${process.env.API_URL}/orgs/${id.toString ()}`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log('getOrgName api method failed for org id ' + id, e.message);
-    sendErrorToZendesk(e);
+    json: true,
+  }).catch (e => {
+    console.log ('getOrgName api method failed for org id ' + id, e.message);
+    sendErrorToZendesk (e);
   });
   if (org) {
     return org.name;
@@ -45,52 +48,52 @@ async function getOrgName(id) {
   return null;
 }
 
-async function getCoach(id) {
-  const coach = await rp({
+async function getCoach (id) {
+  const coach = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/users/' + id.toString(),
+    uri: `${process.env.API_URL}/users/${id.toString ()}`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log('getCoach api method failed for coach id ' + id, e.message);
-    sendErrorToZendesk(e);
+    json: true,
+  }).catch (e => {
+    console.log ('getCoach api method failed for coach id ' + id, e.message);
+    sendErrorToZendesk (e);
   });
   return coach;
 }
 
-async function getClientTasks(id) {
-  const tasks = await rp({
+async function getClientTasks (id) {
+  const tasks = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/clients/' + id.toString() + '/tasks',
+    uri: `${process.env.API_URL}/clients/${id.toString ()}/tasks`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log(
+    json: true,
+  }).catch (e => {
+    console.log (
       'getClientTasks api method failed for client id ' + id,
       e.message
     );
-    sendErrorToZendesk(e);
+    sendErrorToZendesk (e);
   });
   return tasks;
 }
 
-async function getAllMedia() {
-  let listOfMedia = await rp({
+async function getAllMedia () {
+  let listOfMedia = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/media',
+    uri: `${process.env.API_URL}/media`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-    }
-  }).catch(e => {
-    console.log('getAllMedia method failed', e.message);
-    sendErrorToZendesk(e);
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+    },
+  }).catch (e => {
+    console.log ('getAllMedia method failed', e.message);
+    sendErrorToZendesk (e);
   });
-  listOfMedia = JSON.parse(listOfMedia);
-  return listOfMedia.filter(media => {
+  listOfMedia = JSON.parse (listOfMedia);
+  return listOfMedia.filter (media => {
     return (
       media.task_id === null &&
       (media.type === 'STORY' || media.type === 'GENERAL_EDUCATION')
@@ -98,245 +101,240 @@ async function getAllMedia() {
   });
 }
 
-async function getViewedMediaIds(id) {
-  let viewedMedia = await rp({
+async function getViewedMediaIds (id) {
+  let viewedMedia = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/clients/' + id.toString() + '/viewed_media',
+    uri: `${process.env.API_URL}/clients/${id.toString ()}/viewed_media`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-    }
-  }).catch(e => {
-    console.log(
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+    },
+  }).catch (e => {
+    console.log (
       'getViewedMediaIds method failed for client id ' + id,
       e.message
     );
-    sendErrorToZendesk(e);
+    sendErrorToZendesk (e);
   });
-  viewedMedia = JSON.parse(viewedMedia);
-  return viewedMedia.map(media => {
+  viewedMedia = JSON.parse (viewedMedia);
+  return viewedMedia.map (media => {
     return media.id;
   });
 }
 
-async function createRequest(userId, taskId) {
-  const request = await rp({
+async function createRequest (userId, taskId) {
+  const request = await rp ({
     method: 'POST',
-    uri: process.env.API_URL + '/requests',
+    uri: `${process.env.API_URL}/requests`,
     body: {
-      status: 'NEEDS_ASSISTANCE',
+      status: STATUS.NEEDS_ASSISTANCE,
       user_id: userId,
-      task_id: taskId
+      task_id: taskId,
     },
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log(
+    json: true,
+  }).catch (e => {
+    console.log (
       'createRequest method failed with user id ' +
         userId +
         ' on task id ' +
         taskId,
       e.message
     );
-    sendErrorToZendesk(e);
+    sendErrorToZendesk (e);
   });
   return request;
 }
 
-async function setRequestByTaskId(clientId, taskId, status) {
+async function setRequestByTaskId (clientId, taskId, status) {
   if (clientId) {
-    const requests = await rp({
+    const requests = await rp ({
       method: 'GET',
-      uri: process.env.API_URL + '/clients/' + clientId + '/requests',
+      uri: `${process.env.API_URL}/clients/${clientId}/requests`,
       headers: {
-        Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+        Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
       },
-      json: true
-    }).catch(e => {
-      console.log('setRequestByTaskId failed', e.message);
-      sendErrorToZendesk(e);
+      json: true,
+    }).catch (e => {
+      console.log ('setRequestByTaskId failed', e.message);
+      sendErrorToZendesk (e);
     });
     for (let i = 0; i < requests.length; i++) {
       const request = requests[i];
       if (request.task_id === taskId) {
-        await rp({
+        await rp ({
           // eslint-disable-line
           method: 'PUT',
-          uri: process.env.API_URL + '/requests/' + request.id,
+          uri: `${process.env.API_URL}/requests/${request.id}`,
           headers: {
-            Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+            Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
           },
           body: {
             status,
             user_id: clientId,
-            task_id: taskId
+            task_id: taskId,
           },
-          json: true
-        }).catch(e => {
-          console.log(
+          json: true,
+        }).catch (e => {
+          console.log (
             'setRequestByTaskId failed: issue in the for loop',
             e.message
           );
-          sendErrorToZendesk(e);
+          sendErrorToZendesk (e);
         });
       }
     }
   }
 }
 
-async function getUserRequests(userId) {
-  const requests = await rp({
+async function getUserRequests (userId) {
+  const requests = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/clients/' + userId + '/requests',
+    uri: `${process.env.API_URL}/clients/${userId}/requests`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log('getUserRequests method failed for user ' + userId, e.message);
-    sendErrorToZendesk(e);
+    json: true,
+  }).catch (e => {
+    console.log ('getUserRequests method failed for user ' + userId, e.message);
+    sendErrorToZendesk (e);
   });
   return requests;
 }
 
-async function getUserMessages(userId) {
-  const messages = await rp({
+async function getUserMessages (userId) {
+  const messages = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/clients/' + userId + '/messages',
+    uri: `${process.env.API_URL}/clients/${userId}/messages`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log('getUserMessages method failed for user ' + userId, e.message);
-    sendErrorToZendesk(e);
+    json: true,
+  }).catch (e => {
+    console.log ('getUserMessages method failed for user ' + userId, e.message);
+    sendErrorToZendesk (e);
   });
   return messages;
 }
 
-async function createMessage(requestId, fromId, toId, messageToSend, topic) {
+async function createMessage (requestId, fromId, toId, messageToSend, topic) {
   const body = {
     text: messageToSend,
     to_user: toId,
     from_user: fromId,
     media_id: null,
     request_id: requestId,
-    timestamp: new Date(),
-    topic: topic || 'NO_TOPIC',
-    responses: null
+    timestamp: new Date (),
+    topic: topic || TOPICS.NO_TOPIC,
+    responses: null,
   };
-  const message = await rp({
+  const message = await rp ({
     method: 'POST',
-    uri: process.env.API_URL + '/messages',
+    uri: `${process.env.API_URL}/messages`,
     body,
     json: true,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-    }
-  }).catch(e => {
-    console.log(
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+    },
+  }).catch (e => {
+    console.log (
       'createMessage method failed for user ' + fromId + ' to ' + toId,
       e.message
     );
-    sendErrorToZendesk(e);
+    sendErrorToZendesk (e);
   });
 
-  if (fromId !== 41) trackMessageSent(body);
+  if (fromId !== 41) trackMessageSent (body);
 
   return message;
 }
 
-async function updateUser(userId, userData) {
-  const user = await rp({
+async function updateUser (userId, userData) {
+  const user = await rp ({
     method: 'PUT',
-    uri: process.env.API_URL + '/clients/' + userId,
+    uri: `${process.env.API_URL}/clients/${userId}`,
     body: userData,
     json: true,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-    }
-  }).catch(e => {
-    console.log('updateUser method failed for user ' + userId, e.message);
-    sendErrorToZendesk(e);
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+    },
+  }).catch (e => {
+    console.log ('updateUser method failed for user ' + userId, e.message);
+    sendErrorToZendesk (e);
   });
   return user;
 }
 
-async function updateTask(id, taskData) {
-  const task = await rp({
+async function updateTask (id, taskData) {
+  const task = await rp ({
     method: 'PUT',
-    uri: process.env.API_URL + '/tasks/' + id,
+    uri: `${process.env.API_URL}/tasks/${id}`,
     body: taskData,
     json: true,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-    }
-  }).catch(e => {
-    console.log('failed to update task with id ' + id, e.message);
-    sendErrorToZendesk(e);
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+    },
+  }).catch (e => {
+    console.log ('failed to update task with id ' + id, e.message);
+    sendErrorToZendesk (e);
   });
   return task;
 }
 
-async function markMediaAsViewed(clientId, mediaId) {
+async function markMediaAsViewed (clientId, mediaId) {
   if (mediaId) {
-    const media = await rp({
+    const media = await rp ({
       method: 'POST',
-      uri:
-        process.env.API_URL +
-        '/clients/' +
-        clientId +
-        '/viewed_media/' +
-        mediaId,
+      uri: `${process.env.API_URL}/clients/${clientId}/viewed_media/${mediaId}`,
       json: true,
       headers: {
-        Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-      }
-    }).catch(e => {
-      console.log(
+        Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+      },
+    }).catch (e => {
+      console.log (
         'failed to mark media as viewed with media id ' + mediaId,
         e.message
       );
-      sendErrorToZendesk(e);
+      sendErrorToZendesk (e);
     });
     return media;
   }
   return null;
 }
 
-async function getUserFromId(id) {
-  const user = await rp({
+async function getUserFromId (id) {
+  const user = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/users/' + id,
+    uri: `${process.env.API_URL}/users/${id}`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
-    }
-  }).catch(e => {
-    console.log('getUserFromId failed to find user id ' + id, e.message);
-    sendErrorToZendesk(e);
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
+    },
+  }).catch (e => {
+    console.log ('getUserFromId failed to find user id ' + id, e.message);
+    sendErrorToZendesk (e);
   });
-  return JSON.parse(user);
+  return JSON.parse (user);
 }
 
-async function getTask(id) {
-  const task = await rp({
+async function getTask (id) {
+  const task = await rp ({
     method: 'GET',
-    uri: process.env.API_URL + '/tasks/' + id,
+    uri: `${process.env.API_URL}/tasks/${id}`,
     headers: {
-      Authorization: 'Bearer ' + process.env.OAUTH_ACCESS_TOKEN
+      Authorization: `Bearer ${process.env.OAUTH_ACCESS_TOKEN}`,
     },
-    json: true
-  }).catch(e => {
-    console.log('getTask failed to find task id ' + id, e.message);
-    sendErrorToZendesk(e);
+    json: true,
+  }).catch (e => {
+    console.log ('getTask failed to find task id ' + id, e.message);
+    sendErrorToZendesk (e);
   });
   return task;
 }
 
 // if there's a user, return api/client/id data, otherwise return null
-async function getUserDataFromDB(platform, userPlatformId) {
-  const clients = await this.getAllClients();
+async function getUserDataFromDB (platform, userPlatformId) {
+  const clients = await this.getAllClients ();
   if (clients === constants.UNAUTHORIZED) {
     return constants.UNAUTHORIZED;
   }
@@ -349,7 +347,7 @@ async function getUserDataFromDB(platform, userPlatformId) {
       platform === constants.SMS &&
       client.phone !== null &&
       (client.phone === userPlatformId ||
-        formatPhoneNumber(client.phone) === userPlatformId)
+        formatPhoneNumber (client.phone) === userPlatformId)
     ) {
       client.phone = userPlatformId;
       return client;
@@ -360,7 +358,7 @@ async function getUserDataFromDB(platform, userPlatformId) {
     if (
       platform === constants.FB &&
       (client.phone === userPlatformId ||
-        formatPhoneNumber(client.phone) === userPlatformId)
+        formatPhoneNumber (client.phone) === userPlatformId)
     ) {
       return client;
     }
@@ -368,11 +366,11 @@ async function getUserDataFromDB(platform, userPlatformId) {
   return null;
 }
 
-function formatPhoneNumber(unformattedNumber) {
+function formatPhoneNumber (unformattedNumber) {
   if (!unformattedNumber) {
     return null;
   }
-  const digitsOnlyNumber = unformattedNumber.replace(/\D/g, '');
+  const digitsOnlyNumber = unformattedNumber.replace (/\D/g, '');
   if (digitsOnlyNumber.length === 10) {
     return '+1' + digitsOnlyNumber;
   } else if (digitsOnlyNumber.length === 11 && digitsOnlyNumber[0] === '1') {
@@ -381,12 +379,12 @@ function formatPhoneNumber(unformattedNumber) {
   return null;
 }
 
-function sendErrorToZendesk(error) {
-  sgMail.send({
+function sendErrorToZendesk (error) {
+  sgMail.send ({
     to: 'support@helloroo.zendesk.com',
     from: 'no-reply@helloroo.org',
     subject: 'Roo bot error',
-    text: `An error occurred on the bot server: \n ${error}`
+    text: `An error occurred on the bot server: \n ${error}`,
   });
 }
 
@@ -407,5 +405,5 @@ module.exports = {
   updateTask,
   markMediaAsViewed,
   getTask,
-  getUserDataFromDB
+  getUserDataFromDB,
 };
--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,37 @@
+const TOPICS = {
+  NO_TOPIC: 'NO_TOPIC',
+  ULTIMATE_DONE: 'ultimatedone',
+  SETUP_FB: 'setupfb',
+  HELP_USER_RESPONSE: 'helpuserresponse',
+  HELP_USER_CONFIRM: 'helpuserconfirm',
+  WELCOME: 'welcome',
+  INTRO_TASK: 'introtask',
+  CONTENT: 'content',
+  RECURRING: 'recurring',
+  WELCOME_WAIT: 'welcomewait',
+  WELCOME_NEXT: 'welcomenext',
+  NEXT_TOPIC: 'nextTopic',
+  HELP: 'help',
+  RANDOM: 'random',
+  FOLLOW_UP: 'followup',
+  CHECK_IN: 'checkin',
+};
+
+const STATUS = {
+  NEEDS_ASSISTANCE: 'NEEDS_ASSISTANCE',
+  ACTIVE: 'ACTIVE',
+  WORKING: 'WORKING',
+  AWAITING_HELP: 'AWAITING_HELP',
+  COMPLETED: 'COMPLETED',
+};
+
 module.exports = {
   FB: 'fb',
   SMS: 'sms',
   WORKPLAN_URL: 'https://www.helloroo.org/my-tasks',
   INTRO_VIDEO_URL: 'https://youtu.be/WpHtdkKQz8Q',
   DEFAULT_ERR_MESSAGE: 'This message should not be showing up and is an error on our part.',
-  UNAUTHORIZED: 'unauthorized oauth token. Perhaps refresh the bot oauth token?'
+  UNAUTHORIZED: 'Unauthorized oauth token. Perhaps refresh the bot oauth token?',
+  TOPICS,
+  STATUS,
 };
