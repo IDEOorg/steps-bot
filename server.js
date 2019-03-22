81525877ec97acaed87ab84a512d95f3160918a5
sulenchy <sulaimanabiodun172@gmail.com>
2019-03-22 16:56:55 +0100

    feat(auth-helpresponse): add auth restriction to /helpresponse route
    - setup auth0 jwt in server.js
    - add auth0 jwt to /helpresponse route
    
    [Finishes #161305149]

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const jwt = require('express-jwt');
+const { expressJwtSecret } = require('jwks-rsa');
 const cors = require('cors');
 const swaggerUI = require('swagger-ui-express');
 const express = require('express');
@@ -9,7 +11,49 @@ const YAML = require('yamljs');
 
 const swaggerDocument = YAML.load('./swagger.yaml');
 
-module.exports = async function server(
+// Auth0 Config
+const { AUTH0_AUDIENCE, AUTH0_DOMAIN } = process.env;
+const AUTH0_ISSUER = `https://${AUTH0_DOMAIN}/`;
+
+/**
+ * @description Checks the request headers for an auth token.
+ * This is used to secure routes that require authorization.
+ */
+const checkJwt = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({
+      status: 'error',
+      message: 'you are unauthorized to view this resource',
+    });
+  }
+
+  // Authentication middleware. Please see:
+  // https://auth0.com/docs/quickstart/backend/nodejs
+  // for implementation details
+  jwt({
+    // Retrieve the signing key from the server
+    secret: expressJwtSecret({
+      cache: true,
+      rateLimit: true,
+      jwksRequestsPerMinute: 5,
+      jwksUri: `${AUTH0_ISSUER}.well-known/jwks.json`,
+      handleSigningKeyError: (error, callback) => {
+        return callback(error);
+      },
+    }),
+
+    // Validate the audience of the issuer
+    audience: AUTH0_AUDIENCE || 'http://steps-admin.herokuapp.com',
+    issuer: AUTH0_ISSUER,
+    algorithms: ['RS256'],
+    complete: true,
+    requestProperty: 'token',
+  });
+
+  return next();
+};
+
+module.exports = function server(
   fbEndpoint,
   twilioReceiveSmsController,
   getCoachResponse,
@@ -36,7 +80,7 @@ function routes(
   getCoachResponse,
   testTwilioCredentials
 ) {
-  app.get('/helpresponse', getCoachResponse);
+  app.get('/helpresponse', checkJwt, getCoachResponse);
   app.post('/facebook/receive', fbEndpoint);
   app.post('/sms/receive', twilioReceiveSmsController);
   app.post('/sms/test', testTwilioCredentials);
